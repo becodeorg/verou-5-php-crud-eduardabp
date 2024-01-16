c5f97b4f2307d38618005cf33a3b930b92f04584
Eduarda <eduardabpetry@gmail.com>
2024-01-16 10:39:33 +0100

    Copy starter pack

---
+++ classes/CardRepository.php
@@ -0,0 +1,53 @@
+<?php
+
+// This class is focussed on dealing with queries for one type of data
+// That allows for easier re-using and it's rather easy to find all your queries
+// This technique is called the repository pattern
+class CardRepository
+{
+    private DatabaseManager $databaseManager;
+
+    // This class needs a database connection to function
+    public function __construct(DatabaseManager $databaseManager)
+    {
+        $this->databaseManager = $databaseManager;
+    }
+
+    public function create(): void
+    {
+
+    }
+
+    // Get one
+    public function find(): array
+    {
+
+    }
+
+    // Get all
+    public function get(): array
+    {
+        // TODO: Create an SQL query
+        // TODO: Use your database connection (see $databaseManager) and send your query to your database.
+        // TODO: fetch your data at the end of that action.
+        // TODO: replace dummy data by real one
+        return [
+            ['name' => 'dummy one'],
+            ['name' => 'dummy two'],
+        ];
+
+        // We get the database connection first, so we can apply our queries with it
+        // return $this->databaseManager->connection-> (runYourQueryHere)
+    }
+
+    public function update(): void
+    {
+
+    }
+
+    public function delete(): void
+    {
+
+    }
+
+}
---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+// NEVER commit any real passwords
+// TODO: exclude this file from your Github repository for better security
+// There is a specific technique to ignore files in git, which we've talked about
+
+$config = [
+    'host' => '',
+    'user' => '',
+    'password' => '',
+    'dbname' => '',
+];
